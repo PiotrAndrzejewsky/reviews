c65f1b84bb2609c1a93def5c40e7f38347d14e13
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2023-03-08 14:53:19 +0100

    refactor: change email message

--- src/main/java/com/example/reviews/email/EmailService.java
+++ src/main/java/com/example/reviews/email/EmailService.java
@@ -2,9 +2,13 @@ package com.example.reviews.email;
 
 import com.example.reviews.security.JWTVerifierBean;
 import com.example.reviews.user.UserEntity;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.regex.Matcher;
@@ -19,18 +23,33 @@ public class EmailService {
 
     @Autowired
     private JWTVerifierBean jWTVerifierBean;
+
+    @Value("${app.frontend-url}")
+    private String url;
+
     @Autowired
     public EmailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
     public void sendActivationCode(UserEntity user) {
-        String activationCode = jWTVerifierBean.createToken(user.getUsername(), user.getUserId().toString());;
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo(user.getEmail());
-        msg.setSubject("Activation Code");
-        msg.setText("Your activation code is: " + activationCode);
-        javaMailSender.send(msg);
+        String activationCode = jWTVerifierBean.createToken(user.getUsername(), user.getUserId().toString());
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = null;
+        String link = url + "/activate?token=";
+        try {
+            helper = new MimeMessageHelper(message, true);
+            helper.setTo(user.getEmail());
+            helper.setSubject("Activation Code");
+
+            String htmlBody = "<p>Please click on the following link to activate your account:</p><p><a href=\"" + link + activationCode + "\">Activate</a></p>";
+            helper.setText(htmlBody, true);
+            System.out.println("dziala");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+        javaMailSender.send(message);
     }
 
     public boolean isEmailValid(String email) {
